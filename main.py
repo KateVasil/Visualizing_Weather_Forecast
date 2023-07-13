2fb2703b2dc499c9eb7438e4be7a8f1162564c0d
Katerina <katevasil2@gmail.com>
2023-07-13 10:46:37 +0300

    Day 32: Filter data for forecasted days and Tmp/Sky conditions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
@@ -10,15 +10,8 @@ option = st.selectbox("Select data to view"
                       , ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+d, t = get_data(place, forecasted_days, option)
 
-def get_data(days):
-    dates = ["2022-24-10", "2022-25-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
 
 figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperature (C)"})
 st.plotly_chart(figure)
